d716c4cb4be5bd3003393b7126a36f3421b45528
JoaoCher <joaocher@gmail.com>
2020-01-18 20:36:26 -0300

    commit v 1.7 (update App.js)

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import api from './services/api'
 
 import './global.css'
 import './App.css'
@@ -6,17 +7,50 @@ import './Sidebar.css'
 import './Main.css'
 
 function App() {
+  const [devs, setDevs] = useState([])
+
   const [github_username, setGithubUsername] = useState('')
   const [techs, setTechs] = useState('')
   const [latitude, setLatitude] = useState('')
   const [longitude, setLongitude] = useState('')
 
-  useEffect(() => { 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+        setLatitude(latitude)
+        setLongitude(longitude)
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 30000
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs')
+      setDevs(response.data)
+    }
+    loadDevs()
   }, [])
 
   async function handleAddDev(e) {
     e.preventDefault()
-    
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    setGithubUsername('')
+    setTechs('')
+    setDevs([...devs, response.data])
   }
 
   return (
@@ -79,50 +113,20 @@ function App() {
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/20938027?v=4" alt="João Pedro" />
-              <div className="user-info">
-                <strong>João Pedro</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Biografia. Biografia. Biografia. Biografia. Biografia. Biografia.</p>
-            <a href="https://github.com/jpchebs">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/20938027?v=4" alt="João Pedro" />
-              <div className="user-info">
-                <strong>João Pedro</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Biografia. Biografia. Biografia. Biografia. Biografia. Biografia.</p>
-            <a href="https://github.com/jpchebs">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/20938027?v=4" alt="João Pedro" />
-              <div className="user-info">
-                <strong>João Pedro</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Biografia. Biografia. Biografia. Biografia. Biografia. Biografia.</p>
-            <a href="https://github.com/jpchebs">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/20938027?v=4" alt="João Pedro" />
-              <div className="user-info">
-                <strong>João Pedro</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Biografia. Biografia. Biografia. Biografia. Biografia. Biografia.</p>
-            <a href="https://github.com/jpchebs">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+            </li>
+          ))}
+
         </ul>
       </main>
     </div>
